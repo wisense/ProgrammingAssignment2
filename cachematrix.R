e1fb575180226504877f610e1d64829061cc963c
wisense <wisense@2008.sina.com>
2014-04-27 21:41:54 +0800

    make the R file do the right job
    Fill the function contents to make this R file do the right job, that is generate a special 'matrix', and cache its inverse matrix. If the inverse matrix is already in the cache, just retrieve it and do not do any computation.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Generate a special 'matrix', and cache its inverse matrix. 
+## If the inverse matrix is already in the cache, just retrieve it and do not do any computation.
 
-## Write a short comment describing this function
+## makeCacheMatrix function is used to generate a special 'matrix' and cache its inverse matrix,
+## if the inverse matrix is exist.
 
 makeCacheMatrix <- function(x = matrix()) {
+  inv <- NULL 
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() x      ## get the matrix x 
+  setInverse <- function(solve) inv <<- solve
+  getInverse <- function() inv
+  list(set = set, get = get,
+       setInverse = setInverse,
+       getInverse = getInverse)
 
 }
 
 
-## Write a short comment describing this function
+## If the inverse matrix of input is in the cache, return it, otherwise get the inverse matrix and cache it.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+## Return a matrix that is the inverse of 'x'
+  inv <- x$getInverse()     ## query the matrix x's cache
+  if (!is.null(inv)) {      ## check if there is a cache
+    message("getting cached data")
+    return(inv)             ## return the cache if it's exist, don't do computation
+  }
+  data <- x$get()           ## get the matrix x to prepare for computation
+  inv <- solve(data, ...)   ## compute the inverse matrix of x
+  x$setInverse(inv)         ## save the result back to x's cache
+  inv                       ## return the result inverse matrix
 }
